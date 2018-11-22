596b12f15f57e5c78964cbc272fa919b870700c1
wesleyrs <wesley0807@gmail.com>
2018-11-22 01:07:11 -0200

    Encapsulando com o Webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const modoDev = process.env.NODE_ENV !== "production"
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  mode: modoDev ? "development" : "production",
+  entry: "./src/pokemon.js",
+  output: {
+    filename: "app.js",
+    path: __dirname + "/public",
+    },
+  plugins: [
+    new CopyWebpackPlugin ([
+      {context: "src/", from: "**/*.html"},
+      {context: "src/", from: "img/**/*"}
+    ]),
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({cache: true, parallel: true}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  devServer: {
+    contentBase: "./public",
+    port: 9000,
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader,
+      // "style-loader",
+      "css-loader"]
+    }, {
+      test: /\.(jpg|gif|svg|png)$/,
+      use: ["file-loader"]
+    }, {
+      test: /\.html$/,
+      use: ["html-loader"]
+    }]
+  }
+}
