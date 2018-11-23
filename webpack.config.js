4720ab1c645a2cee628915f1ddf47d853e2430d5
wesleyrs <wesley0807@gmail.com>
2018-11-23 02:05:02 -0200

    add font

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,16 @@ module.exports = {
     }, {
       test: /\.html$/,
       use: ["html-loader"]
-    }]
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{
+          loader: 'file-loader',
+          options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+          }
+      }]
+  }
+  ]
   }
 }
